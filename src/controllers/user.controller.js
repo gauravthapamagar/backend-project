e2797cebac2fb4f3f91e3f344b943d62422456bf
Gaurav Thapa <gauaravthapa926@gmail.com>
2025-02-27 23:46:41 +0545

    minor changes on user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -301,7 +301,7 @@ const refreshAccessToken = asyncHandler(async(req, res) => {
 //change user password
 const changeUserPassword = asyncHandler(async(req,res) => {
     const {oldPassword, newPassword} = req.body
-    const User = await User.findById(req.user?._id)
+    const user = await User.findById(req.user?._id)
     const isPasswordCorrect = await user.isPasswordCorrect(oldPassword)
     if(!isPasswordCorrect){
         throw new ApiError(400, "Invalid old password")
